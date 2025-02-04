5129c7135a3dae71bf01e449170b90bebf744ce1
Jhon Barry Gutang <jbgutang771@gmail.com>
2025-02-04 19:57:23 +0800

    added specific layout for specific page

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,7 +1,12 @@
+import Navbar from '@/components/navbar'
+
 export default function About() {
     return (
         <div>
+            <Navbar/>
+            <div>
             This is About Us
+            </div>
         </div>
     )
 }
--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,7 +1,11 @@
+import Navbar from '@/components/navbar'
+
 export default function HomePage() {
     return (
         <div>
+            <div>
             this is the home page here where people can post and comment
+            </div>
         </div>
     )
 }
