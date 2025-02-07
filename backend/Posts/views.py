6fc2103541b12afec8c70decaa58a44b0589ea89
Jhon Barry Gutang <jbgutang771@gmail.com>
2025-02-07 20:50:34 +0800

    modified the view to fit for the response sent in the frontend

--- backend/Posts/views.py
+++ backend/Posts/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, serializers
 from django.core.exceptions import ValidationError
+from rest_framework.response import Response
 from .serializers import PostsSerializer
 from .models import Posts
 
@@ -12,3 +13,10 @@ class PostListCreateView(generics.ListCreateAPIView):
             serializer.save()
         except ValidationError as e:
             raise serializers.ValidationError({"error": str(e)})
+    
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        updated_queryset = self.get_queryset()
+        updated_data = PostsSerializer(updated_queryset, many=True).data
+        return Response(updated_data)
+
