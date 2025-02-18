2eaece133975df348ddf86005baccb98dbb0a276
Jhon Barry Gutang <147809859+JhonGutang@users.noreply.github.com>
2025-02-18 22:19:36 +0800

    Update Hero.tsx

--- frontend/src/components/landing/Hero.tsx
+++ frontend/src/components/landing/Hero.tsx
@@ -11,7 +11,6 @@ const Hero = () => {
 
   return (
     <div className="h-[70vh] flex overflow-hidden">
-      {/* Left Side */}
       <div className="h-full w-2/3 flex flex-col gap-5 justify-center items-center">
         <div>
           <motion.div
@@ -72,12 +71,11 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* Right Side - Animated Image */}
       <div className="h-full w-full flex">
         <motion.div
-          initial={{ opacity: 0, x: 50 }} // Starts off-screen to the right
-          animate={{ opacity: 1, x: 0 }} // Moves into view
-          transition={{ duration: 1 }} // Smooth animation
+          initial={{ opacity: 0, x: 50 }} 
+          animate={{ opacity: 1, x: 0 }} 
+          transition={{ duration: 1 }} 
         >
           <img
             src="https://img.freepik.com/free-vector/business-team-brainstorm-idea-lightbulb-from-jigsaw-working-team-collaboration-enterprise-cooperation-colleagues-mutual-assistance-concept-pinkish-coral-bluevector-isolated-illustration_335657-1651.jpg?t=st=1739883939~exp=1739887539~hmac=0b9bd6df334650b5f334d792122f042fa7e5a58ea27d2648daaedfe228ac2a5f&w=996"
