95a261a1bab8ed43f28b437eaff3e710c9b8b85b
Jhon Barry Gutang <jbgutang771@gmail.com>
2025-02-18 22:17:15 +0800

    improved ui in landing page with navigation, hero and footer. Also added animation to look nice

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,25 +1,19 @@
 'use client';
-import Navbar from '@/components/Navbar'
-import { useEffect, useState } from "react";
-import axiosInstance from '@/utils/axiosInstance';
+import Footer from '@/components/landing/Footer';
+import Hero from '@/components/landing/Hero';
+import Navbar from '@/components/landing/Navbar'
+
 
 interface ApiResponse {
   message: string
 }
 
 export default function Home() {
-  const [data, setData] = useState<ApiResponse | null> (null);
-
-  useEffect(() => {
-    axiosInstance.get("http://127.0.0.1:8000/api/")
-      .then(response => setData(response.data))
-      .catch(error => console.error("Error fetching data:", error));
-  }, []);
   return (
-    <div>
+    <div className='bg-white h-screen text-black'>
       <Navbar/>
-         <h1>Fetching Data from Django</h1>
-         <p>{data ? data.message : "Loading..."}</p>
+      <Hero/>
+      <Footer/>
     </div>
   );
 }
---
+++ frontend/src/components/landing/Footer.tsx
@@ -0,0 +1,73 @@
+import { Avatar, TextField } from "@mui/material";
+import { faEnvelope, faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const Footer = () => {
+  return (
+    <div
+      className="h-[20vh] flex justify-between text-white px-10 gap-15"
+      style={{ backgroundColor: "#388DDC" }}
+    >
+      {/* copyrights */}
+      <div className="h-full flex flex-col justify-center items-center">
+        <div className="flex items-center h-fit">
+          <Avatar src="/logo.png" sx={{ width: "80px", height: "80px" }} />
+          <div className="text-xl">CollabIT CCTC</div>
+        </div>
+        <div className="text-sm">@2025 All Rights Reserved</div>
+      </div>
+
+      {/* footer navigation */}
+      <div className="flex flex-col gap-2 justify-center">
+        <div>Home</div>
+        <div>Community</div>
+        <div>About Us</div>
+      </div>
+
+      {/* contact information */}
+      <div className="flex flex-col gap-2 justify-center">
+        <div className="flex">
+          <FontAwesomeIcon
+            icon={faEnvelope}
+            className="me-3"
+            style={{ width: "20px", height: "20px" }}
+          />
+          <div>jhonbarrygutang@gmail.com</div>
+        </div>
+        <div className="flex">
+          <FontAwesomeIcon
+            icon={faLocationDot}
+            className="me-3"
+            style={{ width: "20px", height: "20px" }}
+            />
+            <div>Cabitoonan, Toledo City, Cebu</div>
+        </div>
+      </div>
+
+      {/* User Interaction */}
+
+      <div className="flex flex-col justify-center gap-2">
+        <div className="flex gap-5">
+          <FontAwesomeIcon
+            icon={faFacebook}
+            style={{ width: "30px", height: "30px" }}
+          />
+          <FontAwesomeIcon
+            icon={faGithub}
+            style={{ width: "30px", height: "30px" }}
+          />
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            style={{ width: "30px", height: "30px" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
