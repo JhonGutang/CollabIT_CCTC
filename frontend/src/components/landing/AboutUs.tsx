38965759ab79c46d46ac70904fbb4a59a101a041
Jhon Barry Gutang <jbgutang771@gmail.com>
2025-02-21 13:01:41 +0800

    added community page with intersection observer for smooth transition when component is rendered in specific viewport height

---
+++ frontend/src/components/landing/AboutUs.tsx
@@ -0,0 +1,9 @@
+const AboutUs = () => {
+    return ( <div className="h-screen bg-white w-full p-10">
+        <div className="text-2xl font-bold">
+            About Us
+        </div>
+    </div> );
+}
+ 
+export default AboutUs;
